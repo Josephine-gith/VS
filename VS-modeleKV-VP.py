d66e53932ac7455251a3c84014bf940e0ed52bd1
Josephine-gith <josephine.peronne@gmail.com>
2026-01-08 10:38:15 +0100

    chgt mineur VP

--- VS-modeleKV-VP.py
+++ VS-modeleKV-VP.py
@@ -53,7 +53,7 @@ def fixe_Bn(h0, Bn, rho=1e3, g=9.81):
     return Bn * rho * g * h0
 
 
-def fixe_Ga(h0, Ga, rho=1e3, g=9.81, uc=1):  # QUELLE VAL DE UC ??????????
+def fixe_Ga(h0, Ga, rho=1e3, g=9.81, uc=1):  # Quelle val de Uc ?
     return rho * g * h0**2 / (uc * Ga)
 
 
@@ -63,7 +63,7 @@ Sim_plas = np.zeros((len(H0), 4))
 
 for i, h0 in enumerate(H0):
     Sim_visc[i] = r_fin(h0=h0, tau0=fixe_Bn(h0, 0.003), k=50)
-    Sim_plas[i] = r_fin(h0=h0, k=fixe_Ga(h0, 10), tau0=1)
+    Sim_plas[i] = r_fin(h0=h0, k=10, tau0=20)
 
 print(Sim_visc, Sim_plas)
 
@@ -74,6 +74,7 @@ plt.title("Bn=0.003, régime visqueux")
 plt.xlabel("(Ga * h0 / r0) ** (1 / 5)")
 plt.ylabel("r_fin/r0 - 1")
 plt.grid()
+
 plt.subplot(1, 2, 2)
 plt.scatter(Sim_plas[:, 1], Sim_plas[:, 2])
 plt.title("Ga=50, régime plastique")
